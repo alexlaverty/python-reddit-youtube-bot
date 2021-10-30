f4a4f201e33828da252eb1b51d0ca20a03dea432
Alex Laverty <alaverty@gmail.com>
2021-10-31 00:34:47 +1100

    Speech resolve acronyms to words

--- speech.py
+++ speech.py
@@ -1,11 +1,33 @@
 import boto3 
 import config
 
+def process_speech_text(text):
+    text = text.replace(" AFAIK ", " as far as I know ")
+    text = text.replace(" AITA ", " Am I The Asshole? ")
+    text = text.replace(" AMA ", " Ask me anything ")
+    text = text.replace(" ELI5 ", " Explain Like I'm Five ")
+    text = text.replace(" IAMA ", " I am a ")
+    text = text.replace(" IANAD ", " i am not a doctor ")
+    text = text.replace(" IANAL ", " i am not a lawyer ")
+    text = text.replace(" IMO ", " in my opinion ")
+    text = text.replace(" NSFL ", " Not safe for life ")
+    text = text.replace(" NSFW ", " Not safe for Work ")
+    text = text.replace(" NTA ", " Not The Asshole ")
+    text = text.replace(" SMH ", " Shaking my head ")
+    text = text.replace(" TD;LR ", " too long didn't read ")
+    text = text.replace(" TDLR ", " too long didn't read ")
+    text = text.replace(" TIL ", " Today I Learned ")
+    text = text.replace(" YTA ", " You're the asshole ")
+    text = text.replace(" SAHM ", " stay at home mother ")
+    return text
+
 def create_audio(path, text):
     print("========== Creating Audio File From Text ==========")   
     print("Path : " + path)
-    print("Speech : " + path)
-    print(text)
+    
+    text = process_speech_text(text)
+    print("Speech : " + text)
+
     polly_client = boto3.Session(
                     aws_access_key_id=config.aws_access_key_id,                     
                     aws_secret_access_key=config.aws_secret_access_key,
