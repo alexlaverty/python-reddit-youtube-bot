c95a397ae73c87bfc47c70d7e51155e72c84023b
Nathon Fowlie <2831833+nathonfowlie@users.noreply.github.com>
2023-05-02 23:20:00 +1000

    style: fix linting errors on publish module
    - Fixed linting issues found by flake8, pylint and black in
      the publish module.
    - Fixed some missing type hints, but didn't resolve all issues
      found by mypy.
    
    refs:
      - relates to #17

--- publish/login.py
+++ publish/login.py
@@ -1,27 +1,42 @@
+"""Login module."""
 import json
 from typing import Dict, List
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.ui import WebDriverWait
-
-""" Login module """
 
 
 def domain_to_url(domain: str) -> str:
-    """ Converts a (partial) domain to valid URL """
-    if domain == '.chrome.google.com/robots.txt':
-        domain = 'chrome.google.com/robots.txt'
+    """Convert a partial domain to valid URL.
+
+    Args:
+        domain: Domain to be converted.
+
+    Returns:
+        A fully qualified URL.
+    """
+    if domain == ".chrome.google.com/robots.txt":
+        domain = "chrome.google.com/robots.txt"
     else:
         if domain.startswith("."):
-            domain = "www" + domain
-    return "http://" + domain
+            domain = f"www{domain}"
+    return f"http://{domain}"
 
 
-def login_using_cookie_file(driver: WebDriver, cookie_file: str):
-    """Restore auth cookies from a file. Does not guarantee that the user is logged in afterwards.
-    Visits the domains specified in the cookies to set them, the previous page is not restored."""
+def login_using_cookie_file(driver: WebDriver, cookie_file: str) -> None:
+    """Restore auth cookies from a file.
+
+    Does not guarantee that the user is logged in afterwards.
+
+    Visits the domains specified in the cookies to set them, the previous page
+    is not restored.
+
+    Args:
+        driver: Selenium webdriver.
+        cookie_file: Existing, valid authentication cookie.
+    """
     domain_cookies: Dict[str, List[object]] = {}
     with open(cookie_file) as file:
         cookies: List = json.load(file)
@@ -39,14 +54,25 @@ def login_using_cookie_file(driver: WebDriver, cookie_file: str):
             cookie.pop("storeId", None)  # Firefox container attribute
             try:
                 driver.add_cookie(cookie)
-            except:
+            except Exception:
                 print(f"Couldn't set cookie {cookie['name']} for {domain}")
 
 
 def confirm_logged_in(driver: WebDriver) -> bool:
-    """ Confirm that the user is logged in. The browser needs to be navigated to a YouTube page. """
+    """Confirm that the user is logged in.
+
+    The browser needs to be navigated to a YouTube page.
+
+    Args:
+        driver: Selenium webdrive.
+
+    Returns:
+        `True` if the user is logged in, otherwise `False`.
+    """
     try:
-        WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.ID, "avatar-btn")))
+        WebDriverWait(driver, 5).until(
+            ec.element_to_be_clickable((By.ID, "avatar-btn"))
+        )
         return True
     except TimeoutError:
         return False
